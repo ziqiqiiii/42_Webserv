69b49d919334c1eb208f5f62b1d65ba92deb5028
ziqiqiiii <ziqi.thong@gmail.com>
2025-01-16 23:58:02 +0800

    chore - add comment for Utils class

--- includes/Utils/Utils.hpp
+++ includes/Utils/Utils.hpp
@@ -9,14 +9,36 @@
 # include <csignal>
 # include <vector>
 
+# include "../../includes/Logger/Logger.hpp"
+
 using std::cout;
 using std::endl;
 using std::string;
 
+/**
+ * @namespace WebServer
+ * @brief Contains all components related to the web server.
+ */
 namespace WebServer
 {
+       /**
+     * @class Utils
+     * @brief A utility class providing static helper functions for common tasks.
+     *
+     * The Utils class offers utility functions for tasks such as handling signals and
+     * splitting strings. All methods are static, and the class cannot be instantiated.
+     *
+     * Key features include:
+     * - Signal handling for the server.
+     * - String manipulation utilities.
+     */
     class Utils
     {
+        private:
+            Utils();
+            ~Utils();
+            Utils(const Utils& other);
+            Utils& operator=(const Utils& other);
         public:
             static  void signalHandler(int signum);
             static  std::vector<string> splitString(const string& s, const string&del);
--- srcs/Utils/Utils.cpp
+++ srcs/Utils/Utils.cpp
@@ -1,12 +1,59 @@
 # include "../includes/Utils/Utils.hpp"
-# include "../includes/Logger/Logger.hpp"
 
+/**
+ * @brief Default constructor for Utils.
+ * Private to prevent instantiation.
+ */
+WebServer::Utils::Utils() {}
+
+/**
+ * @brief Destructor for Utils.
+ * Private to prevent deletion of instances.
+ */
+WebServer::Utils::~Utils() {}
+
+/**
+ * @brief Copy constructor for Utils.
+ * Private to prevent copying of the Utils class.
+ *
+ * @param other The Utils object to copy from.
+ */
+WebServer::Utils::Utils(const Utils& other) {*this = other; }
+
+/**
+ * @brief Assignment operator for Utils.
+ * Private to prevent assignment of the Utils class.
+ *
+ * @param other The Utils object to assign from.
+ * @return Utils& A reference to the updated Utils object.
+ */
+WebServer::Utils& WebServer::Utils::operator=(const Utils& other) { return *this; }
+
+/**
+ * @brief Handles signals received by the server.
+ *
+ * Logs the received signal and exits the application.
+ *
+ * @param signum The signal number to handle (e.g., SIGINT, SIGTERM).
+ */
 void WebServer::Utils::signalHandler(int signum)
 {
-    Logger::logMsg(RED, "Interrup signal (%d) received.\n", signum);
+    WebServer::Logger *logManager = WebServer::Logger::getInstance();
+
+    logManager->logMsg(RED, "Interrup signal (%d) received.\n", signum);
     exit(signum);
 }
 
+/**
+ * @brief Splits a string into a vector of substrings based on a specified delimiter.
+ *
+ * This method takes an input string and a delimiter, then splits the string into
+ * substrings wherever the delimiter occurs. The substrings are returned as a vector.
+ *
+ * @param s The input string to split.
+ * @param del The delimiter used to split the string. Defaults to a single space (" ").
+ * @return std::vector<string> A vector containing the split substrings.
+ */
 std::vector<string> WebServer::Utils::splitString(const string& s, const string&del = " ")
 {
     std::vector<string> tokens;
