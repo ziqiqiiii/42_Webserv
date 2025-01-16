d21a86570cc4de217968dc9795376d2f3be7b6fe
ziqiqiiii <ziqi.thong@gmail.com>
2025-01-16 23:49:48 +0800

    feat(update) - change Logger to Singleton

--- srcs/Logger/Logger.cpp
+++ srcs/Logger/Logger.cpp
@@ -1,6 +1,74 @@
 # include "../../includes/Logger/Logger.hpp"
 
-void	Logger::logMsg(const char *color, const char* msg, ...)
+/**
+ * @brief Default constructor for Logger.
+ * Initializes a Logger object. Private to enforce Singleton design.
+ */
+WebServer::Logger::Logger() {}
+
+/**
+ * @brief Destructor for Logger.
+ * Cleans up resources used by the Logger object. Private to enforce Singleton design.
+ */
+WebServer::Logger::~Logger() {}
+
+/**
+ * @brief Copy constructor for Logger.
+ *
+ * Copies the instance pointer from another Logger object.
+ * 
+ * @param other The Logger object to copy from.
+ */
+WebServer::Logger::Logger(const Logger &other) { instancePtr = other.instancePtr; }
+
+/**
+ * @brief Assignment operator for Logger.
+ *
+ * Assigns the instance pointer from another Logger object.
+ * 
+ * @param other The Logger object to assign from.
+ * @return Logger& A reference to the updated Logger object.
+ */
+WebServer::Logger& WebServer::Logger::operator=(const Logger &other)
+{
+	if (this != &other)
+        instancePtr = other.instancePtr;
+    return *this;
+}
+
+WebServer::Logger* WebServer::Logger::instancePtr = nullptr; /**< Static pointer to the Singleton instance of Logger. */
+std::mutex WebServer::Logger::mtx; /**< Mutex to ensure thread safety during Singleton instance creation. */
+
+/**
+ * @brief Retrieves the Singleton instance of the Logger class.
+ *
+ * Ensures that only one instance of the Logger class exists. If the instance
+ * does not already exist, it creates one in a thread-safe manner.
+ *
+ * @return Logger* Pointer to the Singleton instance of the Logger class.
+ */
+WebServer::Logger* WebServer::Logger::getInstance() {
+	if (instancePtr == nullptr) {
+		std::lock_guard<std::mutex> lock(mtx);
+		if (instancePtr == nullptr) {
+			instancePtr = new WebServer::Logger();
+		}
+	}
+	return instancePtr;
+}
+
+/**
+ * @brief Logs a formatted message to the console with the specified color.
+ *
+ * This method uses ANSI escape codes to print the message in the given color.
+ * It supports printf-style formatting for the message. Timestamps are automatically
+ * appended to each log entry.
+ *
+ * @param color The color code for the message (e.g., `RED`, `CYAN`).
+ * @param msg The format string for the message.
+ * @param ... The arguments for the format string.
+ */
+void	WebServer::Logger::logMsg(const char *color, const char* msg, ...)
 {
 	char		output[8192];
 	va_list		args;
@@ -24,7 +92,15 @@ void	Logger::logMsg(const char *color, const char* msg, ...)
 	cout << color << date << output << RESET << endl;
 }
 
-string Logger::getCurrTime()
+/**
+ * @brief Retrieves the current time as a formatted string.
+ *
+ * The timestamp includes the current date and time in GST (GMT+8). If the hour exceeds 24,
+ * the date is adjusted accordingly.
+ *
+ * @return string The current timestamp in a formatted string.
+ */
+string WebServer::Logger::getCurrTime()
 {
 	char date[50];
 	time_t now = time(0);
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,27 +10,28 @@ int main()
     test.bindConnection();
     test.listenConnection();
     HTTPRequest request;
+    WebServer::Logger *logManager = WebServer::Logger::getInstance();
 
-    Logger::logMsg(LIGHTMAGENTA, "+++++++ Waiting for new connection ++++++++\n");
+    logManager->logMsg(LIGHTMAGENTA, "+++++++ Waiting for new connection ++++++++\n");
     try {
         while (true) {
             new_socket = test.acceptConnection();
-            Logger::logMsg(LIGHTMAGENTA, "+++++++ Connection Accepted ++++++++\n");
+            logManager->logMsg(LIGHTMAGENTA, "+++++++ Connection Accepted ++++++++\n");
 
             char buffer[30000] = {0};
             int valread = read(new_socket, buffer, 30000);
             string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nnihao world!";
 
-            Logger::logMsg(YELLOW, "------- Header -------\n");
-            Logger::logMsg(YELLOW, "%s\n", buffer);
+            logManager->logMsg(YELLOW, "------- Header -------\n");
+            logManager->logMsg(YELLOW, "%s\n", buffer);
             request.parseMessage(buffer);
-            Logger::logMsg(LIGHT_BLUE, "------- Getters -------\n");
-            Logger::logMsg(LIGHT_BLUE, "Start line: %s\n", request.getStarline().c_str());
+            logManager->logMsg(LIGHT_BLUE, "------- Getters -------\n");
+            logManager->logMsg(LIGHT_BLUE, "Start line: %s\n", request.getStarline().c_str());
             cout << LIGHT_BLUE << "Field line:\n" << request.getHeaders() << endl;
-            Logger::logMsg(LIGHT_BLUE, "Message Body: %s\n", request.getBody().c_str());
-            Logger::logMsg(LIGHT_BLUE, "+++++++ Sending Message ++++++++\n");
+            logManager->logMsg(LIGHT_BLUE, "Message Body: %s\n", request.getBody().c_str());
+            logManager->logMsg(LIGHT_BLUE, "+++++++ Sending Message ++++++++\n");
             send(new_socket, hello.c_str(), hello.size(), 0);
-            Logger::logMsg(LIGHT_BLUE, "------------------Hello message sent-------------------%d\n", valread);
+            logManager->logMsg(LIGHT_BLUE, "------------------Hello message sent-------------------%d\n", valread);
             close(new_socket);
             // Reset the socket descriptor
             new_socket = -1;
